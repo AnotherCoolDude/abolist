770fbc8b77ec6fbd299b31a17cd25a0f0f71ffc5
AnotherCoolDude <christian.hovenbitzer@gmx.net>
2019-12-27 16:42:25 +0100

    Initial commit

---
+++ Abolist.svelte
@@ -0,0 +1,18 @@
+<script>
+  let abos = [
+    { name: "Netflix", cost: "10", interval: "monthly" },
+    { name: "Swapfies", cost: "19,50", interval: "monthly" }
+  ];
+</script>
+
+<style>
+  ul {
+    list-style-type: none;
+  }
+</style>
+
+<ul>
+  {#each abos as abo}
+    <li>{abo.name} costs you {abo.cost}€ {abo.interval}</li>
+  {/each}
+</ul>
